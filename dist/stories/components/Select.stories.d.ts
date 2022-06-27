4ace812989e208a37246d07d2152fab942731311
johannpereze <sebas.jsp@gmail.com>
2022-06-27 11:34:03 -0500

    chore: deleted dist folder

--- dist/stories/components/Select.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { Select } from "../../components/select/Select";
-declare const _default: ComponentMeta<typeof Select>;
-export default _default;
-export declare const Outlined: ComponentStory<typeof Select>;
-export declare const Filled: ComponentStory<typeof Select>;
-export declare const Standard: ComponentStory<typeof Select>;
--- dist/stories/components/TextField.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import TextField from '../../components/textField/TextField';
-declare const _default: ComponentMeta<typeof TextField>;
-export default _default;
-export declare const Default: ComponentStory<typeof TextField>;
